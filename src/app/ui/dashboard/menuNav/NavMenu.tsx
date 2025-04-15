035910978096fa88a20cfe545115774192c645df
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-15 19:40:53 +0200

    feat: add burger menu

---
+++ src/app/ui/dashboard/menuNav/NavMenu.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import styles from "./navMenu.module.css";
+export default function NavMenu({
+	openMenu,
+	closeMenu,
+}: { openMenu: boolean; closeMenu: () => void }) {
+	return (
+		<>
+			<section
+				className={`${styles.section} ${openMenu ? styles.isOpen : !openMenu ? styles.isClose : ""}`}
+			>
+				<ul className={styles.ul}>
+					<Link className={styles.link} href="#">
+						Accueil
+					</Link>
+					<Link className={styles.link} href="#">
+						Compagnie
+					</Link>
+					<Link className={styles.link} href="#">
+						Spectacle
+					</Link>
+					<Link className={styles.link} href="#">
+						Mediations
+					</Link>
+					<Link className={styles.link} href="#">
+						Contact
+					</Link>
+				</ul>
+				<button className={styles.button} type="button" onClick={closeMenu}>
+					X
+				</button>
+			</section>
+		</>
+	);
+}
--- src/app/ui/dashboard/navBar/NavBar.tsx
+++ src/app/ui/dashboard/navBar/NavBar.tsx
@@ -1,13 +1,43 @@
-import Link from "next/link";
+"use client";
+
+import { Menu } from "lucide-react";
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./navBar.module.css";
+import { createPortal } from "react-dom";
+import NavMenu from "../menuNav/NavMenu";
 
 export default function NavBar() {
+	const [openMenu, setOpenMenu] = useState(false);
+	const handleClickOpenMenu = () => {
+		setOpenMenu(!openMenu);
+	};
 	return (
 		<>
-			<Link href="#">Accueil</Link>
-			<Link href="#">Accueil</Link>
-			<Link href="#">Accueil</Link>
-			<Link href="#">Accueil</Link>
-			<Link href="#">Accueil</Link>
+			<nav className={styles.nav}>
+				<section>
+					<Menu onClick={handleClickOpenMenu} size={48} />
+					{openMenu &&
+						createPortal(
+							<NavMenu
+								openMenu={openMenu}
+								closeMenu={() => {
+									setOpenMenu(false);
+								}}
+							/>,
+							document.body,
+						)}
+				</section>
+				<section className={styles.image}>
+					<Image
+						src="/images/logo.png"
+						width={1280}
+						height={760}
+						alt="logo compagnie l'enclos"
+						className={styles.logo}
+					/>
+				</section>
+			</nav>
 		</>
 	);
 }
