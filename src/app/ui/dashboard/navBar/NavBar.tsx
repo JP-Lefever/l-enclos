b4b22b5694566c3d7e9b55ffa8e641948d3ad96a
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-15 14:01:05 +0200

    feat: add component navBar

---
+++ src/app/ui/dashboard/navBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NavBar() {
+	return (
+		<>
+			<Link href="#">Accueil</Link>
+			<Link href="#">Accueil</Link>
+			<Link href="#">Accueil</Link>
+			<Link href="#">Accueil</Link>
+			<Link href="#">Accueil</Link>
+		</>
+	);
+}
