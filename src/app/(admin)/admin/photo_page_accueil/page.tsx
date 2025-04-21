c3918ec11ce47775e24ed7f2fb7bf36a337ebeb8
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-21 12:25:19 +0200

    feat: fix apostrophe deployment pb

--- src/app/(admin)/admin/photo_page_accueil/page.tsx
+++ src/app/(admin)/admin/photo_page_accueil/page.tsx
@@ -5,7 +5,7 @@ export default function PhotoAccueil() {
 	return (
 		<>
 			<section className={styles.section}>
-				<h1 className={styles.h1}>Photo page d'accueil</h1>
+				<h1 className={styles.h1}>{"Photo page d'accueil"}</h1>
 				<PhotoHomePage />
 			</section>
 		</>
