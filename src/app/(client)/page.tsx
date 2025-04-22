a1af8ceb96bdfe3b0437f8ef121fd99eb879b3db
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-22 17:33:36 +0200

    feat: add presentation compagnie home page

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,9 +1,16 @@
+import styles from "./page.module.css";
+import PresCompagnie from "@/components/ui/presCompagnie/PresCompagnie";
 import AutoPlay from "@/components/ui/slider/Autoplay";
 
 export default function Home() {
 	return (
 		<>
-			<AutoPlay />
+			<section>
+				<AutoPlay />
+			</section>
+			<section className={styles.sectionPres}>
+				<PresCompagnie />
+			</section>
 		</>
 	);
 }
