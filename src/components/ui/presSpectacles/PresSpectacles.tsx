526b78d2f5d6ba05888bfa18a0c330bccde7cdd3
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-23 09:37:44 +0200

    fix: apostroph deployement

--- src/components/ui/presSpectacles/PresSpectacles.tsx
+++ src/components/ui/presSpectacles/PresSpectacles.tsx
@@ -19,14 +19,14 @@ export default function PresSpectacles() {
 				<article>
 					<h1 className={styles.h1}>Spectales</h1>
 					<p className={styles.paragraph}>
-						"L'Enclos parle de cet Espace Clos qui entoure chaque être humain,
+						{`L'Enclos parle de cet Espace Clos qui entoure chaque être humain,
 						l'intimité de l'être dans toute sa complexité, l'endroit de notre
 						vulnérabilité. Notre carapace. Par ses créations toujours axées sur
 						des écritures contemporaines, L'Enclos souhaite ainsi ouvrir la
 						porte de cet espace clos difficilement accessible dans la « vraie
 						vie » et ainsi aller à la rencontre de ses personnages dans leur
 						profondeur, dans les méandres de leurs pensées, dans leur
-						sensibilité, dans leurs limites…."
+						sensibilité, dans leurs limites….`}
 					</p>
 					<div className={styles.div}>
 						<Link className={styles.link} href="#">
