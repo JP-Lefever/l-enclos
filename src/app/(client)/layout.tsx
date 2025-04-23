84f3d21d24792fa15fdced24565f54dc78c53110
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-23 10:43:47 +0200

    feat: add footer

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -2,6 +2,7 @@ import { paragraphFont } from "../../components/ui/font";
 import styles from "./layout.module.css";
 import NavBar from "../../features/navBar/NavBar";
 import "../globals.css";
+import Footer from "@/components/ui/footer/Footer";
 
 export default function RootLayout({
 	children,
@@ -13,6 +14,7 @@ export default function RootLayout({
 			<body className={`${paragraphFont.className} ${styles.body}`}>
 				<NavBar />
 				<main>{children}</main>
+				<Footer />
 			</body>
 		</html>
 	);
