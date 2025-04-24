c6918a44683be94209cc5627cf2ef3e37540a61b
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-24 12:54:48 +0200

    feat: add section primaire

--- src/app/(client)/mediation/page.tsx
+++ src/app/(client)/mediation/page.tsx
@@ -1,7 +1,15 @@
+import MediationPrimaire from "@/components/ui/mediationPrimaire/MediationPrimaire";
+import styles from "./page.module.css";
+
 export default function MediationPage() {
 	return (
 		<>
-			<h1>Mediation</h1>
+			<section className={styles.section}>
+				<h1 className={styles.h1}>Mediations</h1>
+			</section>
+			<section>
+				<MediationPrimaire />
+			</section>
 		</>
 	);
 }
---
+++ src/components/ui/mediationPrimaire/MediationPrimaire.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import styles from "./mediationPrimaire.module.css";
+import Image from "next/image";
+
+export default function MediationPrimaire() {
+	return (
+		<>
+			<section className={styles.section}>
+				<article>
+					<Image
+						className={styles.image}
+						src={"/images/mediation.webp"}
+						alt={"La compagnie l'Enclos"}
+						width={1080}
+						height={860}
+					/>
+				</article>
+
+				<article>
+					<h1 className={styles.h1}>Théâtre - Primaire</h1>
+					<p className={styles.paragraph}>Ateliers primaires</p>
+					<div className={styles.div}>
+						<Link className={styles.link} href="#">
+							En savoir plus
+						</Link>
+					</div>
+				</article>
+			</section>
+		</>
+	);
+}
