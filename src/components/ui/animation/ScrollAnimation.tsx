9ae63ef247ac8759904fb43d4989f943ee2a5b89
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-04-24 14:41:31 +0200

    feat: add spectacles animation

--- src/components/ui/animation/ScrollAnimation.tsx
+++ src/components/ui/animation/ScrollAnimation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion, useInView } from "framer-motion";
+import React from "react";
 import { type ReactNode, useRef } from "react";
 
 type ScrollAnimationType = {
@@ -10,7 +11,7 @@ type ScrollAnimationType = {
 export const ScrollAnimation = ({ children }: ScrollAnimationType) => {
 	const ref = useRef(null);
 	const isInView = useInView(ref, { once: true, amount: 0.4 });
-	console.log(ref);
+
 	const variants = {
 		hidden: { opacity: 0, y: -20, x: -20 },
 		visible: { opacity: 1, y: 0, x: 0, transition: { duration: 2 } },
@@ -30,7 +31,7 @@ export const ScrollAnimation = ({ children }: ScrollAnimationType) => {
 export const ScrollAnimation2 = ({ children }: ScrollAnimationType) => {
 	const ref = useRef(null);
 	const isInView = useInView(ref, { once: true, amount: 0.4 });
-	console.log(ref);
+
 	const variants = {
 		hidden: { opacity: 0, y: 20, x: 20 },
 		visible: { opacity: 1, y: 0, x: 0, transition: { duration: 2 } },
@@ -45,4 +46,38 @@ export const ScrollAnimation2 = ({ children }: ScrollAnimationType) => {
 			{children}
 		</motion.div>
 	);
+};
+
+export const ScrollAnimation3 = ({ children }: ScrollAnimationType) => {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true, amount: 0.4 });
+
+	const containerVariants = {
+		hidden: {},
+		visible: {
+			transition: {
+				staggerChildren: 0.5,
+			},
+		},
+	};
+
+	return (
+		<motion.div
+			ref={ref}
+			initial="hidden"
+			animate={isInView ? "visible" : "hidden"}
+			variants={containerVariants}
+		>
+			{children}
+		</motion.div>
+	);
+};
+
+export const ScrollAnimation4 = ({ children }: ScrollAnimationType) => {
+	const itemVariants = {
+		hidden: { opacity: 0, y: 20 },
+		visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+	};
+
+	return <motion.div variants={itemVariants}>{children}</motion.div>;
 };
