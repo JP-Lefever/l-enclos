ef6e61aa4504deecba5d9ba46c6a0f68a38728a6
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-05-05 16:45:26 +0200

    fix: clean code unused

--- src/app/(client)/mediation/page.tsx
+++ src/app/(client)/mediation/page.tsx
@@ -3,10 +3,7 @@ import styles from "./page.module.css";
 import MediationCollege from "@/features/mediation/mediationCollege/MediationCollege";
 import MediationEphad from "@/features/mediation/mediationEphad/MediationEphad";
 import ButtonMediation from "@/components/ui/buttonMediation/ButtonMediation";
-import {
-	ScrollAnimation2,
-	ScrollAnimation,
-} from "@/components/ui/animation/ScrollAnimation";
+
 
 export default function MediationPage() {
 	return (
