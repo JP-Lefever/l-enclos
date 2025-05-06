6a8f9d846d6f1fddd10c15dec3a625d18f854752
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-05-06 17:43:19 +0200

    fix: props contat form

--- src/features/contact/contactForm/Contact.repository.ts
+++ src/features/contact/contactForm/Contact.repository.ts
@@ -4,16 +4,16 @@ import postgres from "postgres";
 const sql = postgres(process.env.POSTGRES_URL as string, { ssl: "require" });
 
 class Contact {
-	async createMessage(data: ContactProps): Promise<ResultProps> {
-		const { firstname, lastname, email, organism, message } = data;
+	async createMessage(data :Omit<ContactProps, "id" | "date" | "is_treated">): Promise<ResultProps> {
+		const { firstname, lastname, email, organism, message,subject } = data;
 
 		function safeValue(value: string | undefined) {
 			return value === undefined ? null : value;
 		}
 		try {
 			await sql`
-        INSERT INTO contact(firstname, lastname, email, organism, message)
-        VALUES(${safeValue(firstname)}, ${safeValue(lastname)}, ${safeValue(email)}, ${safeValue(organism)}, ${safeValue(message)})
+        INSERT INTO contact(firstname, lastname, email, organism, message, subject)
+        VALUES(${safeValue(firstname)}, ${safeValue(lastname)}, ${safeValue(email)}, ${safeValue(organism)}, ${safeValue(message)}, ${safeValue(subject)})
         `;
 			return { success: true, message: "Le message a bien été envoyé" };
 		} catch (error) {
