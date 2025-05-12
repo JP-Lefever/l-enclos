0097116b3ade0496f0fc6b1ac2394f4f934bc4d7
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-05-12 10:53:10 +0200

    feat: add header to agenda Page

--- src/app/(client)/agenda/page.tsx
+++ src/app/(client)/agenda/page.tsx
@@ -1,3 +1,4 @@
+import styles from "./page.module.css"
 import DisplayDate from "@/features/agenda/client/displayDate/DisplayDate";
 import {readAllDateComing} from "@/features/agenda/agenda.action";
 
@@ -6,8 +7,13 @@ export default async function AgendaPage(){
     const {data, error} = await readAllDateComing()
 
     return (<>
+        <section className={styles.section}>
+        <h1 className={styles.h1}>Agenda</h1>
+        </section>
+            <section>
         {!error && data &&
         <DisplayDate datesSpec={data}/>
         }
+            </section>
     </>)
 }
