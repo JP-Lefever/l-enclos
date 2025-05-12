fccdff2520efc4925eefac6c17838413943376b0
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-05-12 14:53:06 +0200

    feat: clean code unused

--- src/app/(client)/agenda/creation/page.tsx
+++ src/app/(client)/agenda/creation/page.tsx
@@ -1,7 +1,7 @@
-import styles from "./page.module.css"
+
 import DisplayDate from "@/features/agenda/client/displayDate/DisplayDate";
 import {readAllDateComing} from "@/features/agenda/agenda.action";
-import {ButtonsDates} from "@/components/ui/buttons/Buttons";
+
 
 export default async function AgendaPage(){
 
