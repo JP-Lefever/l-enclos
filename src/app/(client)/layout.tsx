63a92c2d4b653d287341421a6a70d5b30ade0e91
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2026-03-03 16:21:51 +0100

    feat: add responsive to presSpec component

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -4,6 +4,7 @@ import "../globals.css";
 import NavBar from "@/components/layout/navigation/navBar/NavBar";
 import Footer from "@/components/layout/footer/Footer";
 import React from "react";
+import {paragraphFont, titleFont} from "@/components/ui/font";
 
 
 
@@ -15,7 +16,7 @@ export default function LayoutClient({
 	return (
 		<>
 				<NavBar />
-				<main>{children}</main>
+				<main className={`${paragraphFont.variable}  ${titleFont.variable}`}>{children}</main>
 				<Footer />
 		</>
 	)
